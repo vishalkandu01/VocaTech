0e4de1cc4183186b674434d977276c5009f3acdc
Vishal Kandu <kanduvishal07@gmail.com>
2023-11-15 21:58:20 +0530

    wishMe function is edited

--- vocaTech.py
+++ vocaTech.py
@@ -13,8 +13,19 @@ def speak(audio):
     engine.say(audio) 
     engine.runAndWait() #Without this command, speech will not be audible to us.
 
-def wishme():
+def wishMe():
     hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Jarvis Sir. Please tell me how may I help you")      
+    
 
 def takeCommand():
     #It takes microphone input from the user and returns string output
