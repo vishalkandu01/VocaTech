7c5192514d2c97dc61cdda936642c4d6ce18bec2
Vishal Kandu <kanduvishal07@gmail.com>
2023-11-15 20:49:43 +0530

    created our takeCommand function

--- vocaTech.py
+++ vocaTech.py
@@ -1,6 +1,8 @@
 import pyttsx3
 import datetime
+import speechRecognition as sr
 
+ 
 
 
 engine = pyttsx3.init('sapi5') # sapi5 api helps in synthesis and recognition of voice.
@@ -14,6 +16,14 @@ def speak(audio):
 def wishme():
     hour = int(datetime.datetime.now().hour)
 
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
  
 
 if __name__=="__main__" :
