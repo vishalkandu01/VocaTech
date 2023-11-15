a26e12847cc308cf414c63d6a4a1225f146719e4
Vishal Kandu <kanduvishal07@gmail.com>
2023-11-15 20:44:12 +0530

    we are getting the details of current voice

--- vocaTech.py
+++ vocaTech.py
@@ -1,2 +1,8 @@
+import pyttsx3
+
+engine = pyttsx3.init('sapi5') # sapi5 api helps in synthesis and recognition of voice.
+voices= engine.getProperty('voices') #getting details of current voice
+engine.setProperty('voice', voice[0].id)
+
 def speak(audio):
        pass      #For now, we will write the conditions later.
