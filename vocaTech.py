156ba0d73c5e82edf32d340bf726c374b9768cc9
Vishal Kandu <kanduvishal07@gmail.com>
2023-11-15 20:51:15 +0530

    takeCommand function is completed

--- vocaTech.py
+++ vocaTech.py
@@ -24,6 +24,18 @@ def takeCommand():
         print("Listening...")
         r.pause_threshold = 1
         audio = r.listen(source)
+    
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in') #Using google for voice recognition.
+        print(f"User said: {query}\n")  #User query will be printed.
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")   #Say that again will be printed in case of improper voice 
+        return "None" #None string will be returned
+    
+    return query
  
 
 if __name__=="__main__" :
