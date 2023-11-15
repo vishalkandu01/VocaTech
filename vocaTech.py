fc899b983d966436193f89b3b218967386430667
Vishal Kandu <kanduvishal07@gmail.com>
2023-11-15 20:46:30 +0530

    created our main function

--- vocaTech.py
+++ vocaTech.py
@@ -7,3 +7,8 @@ engine.setProperty('voice', voice[0].id)
 def speak(audio):
     engine.say(audio) 
     engine.runAndWait() #Without this command, speech will not be audible to us.
+
+if __name__=="__main__" :
+    speak("Code With Vishal Kandu")
+
+ 
