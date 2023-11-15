339ed78c0cc6bf287a850ce07d13c0e5a352c270
Vishal Kandu <kanduvishal07@gmail.com>
2023-11-15 20:47:26 +0530

    define the wishme function

--- vocaTech.py
+++ vocaTech.py
@@ -1,4 +1,7 @@
 import pyttsx3
+import datetime
+
+
 
 engine = pyttsx3.init('sapi5') # sapi5 api helps in synthesis and recognition of voice.
 voices= engine.getProperty('voices') #getting details of current voice
@@ -7,6 +10,11 @@ engine.setProperty('voice', voice[0].id)
 def speak(audio):
     engine.say(audio) 
     engine.runAndWait() #Without this command, speech will not be audible to us.
+
+def wishme():
+    hour = int(datetime.datetime.now().hour)
+
+ 
 
 if __name__=="__main__" :
     speak("Code With Vishal Kandu")
