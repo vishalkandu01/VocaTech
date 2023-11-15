c354fb33a0e6582995656ffd4f9899ee03d4ab17
Vishal Kandu <kanduvishal07@gmail.com>
2023-11-15 20:45:24 +0530

    now speech is audible to us

--- vocaTech.py
+++ vocaTech.py
@@ -5,4 +5,5 @@ voices= engine.getProperty('voices') #getting details of current voice
 engine.setProperty('voice', voice[0].id)
 
 def speak(audio):
-       pass      #For now, we will write the conditions later.
+    engine.say(audio) 
+    engine.runAndWait() #Without this command, speech will not be audible to us.
